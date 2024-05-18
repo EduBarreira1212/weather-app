103718a9ff0235cb7eb361b9427ed8c2c7cdb36e
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-18 16:29:45 -0300

    feat: Input component created

---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+const Input = () => {
+    return(
+        <div>
+            <label>Insert a city</label>
+            <input type='text' placeholder='Ex: London'/>
+        </div>
+    );
+}
+
+export default Input;
