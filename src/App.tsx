20d174c4dc09cfe68fd38a82d65af0adcb185db2
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-18 16:38:05 -0300

    feat: using components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import Header from './components/Header';
+import Input from './components/Input';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header/>
+      <Input/>
     </div>
   );
 }
