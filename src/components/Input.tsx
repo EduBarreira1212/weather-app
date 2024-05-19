53d5f0d9f299e919a84700279a3e585304d44bb0
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-19 12:29:17 -0300

    feat: function to click to get geo code created

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,21 @@
+import { useRef, useState } from "react";
+import getGeoCoding from "../services/getGeoCoding";
+
 const Input = () => {
+
+    const cityRef = useRef<HTMLInputElement>(null)
+
+    const [city, setCity] = useState("");
+
+    const handleClick = async () => {
+        setCity(cityRef.current?.value || "");
+        const cityGeoCode = await getGeoCoding(city);
+    }
     return(
         <div>
             <label>Insert a city</label>
-            <input type='text' placeholder='Ex: London'/>
-            <button>Search</button>
+            <input ref={cityRef} type='text' placeholder='Ex: London'/>
+            <button onClick={handleClick}>Search</button>
         </div>
     );
 }
