124a94e78f77de87db9807362577c4c420638eaf
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-19 20:09:58 -0300

    feat: useSelect logic to show city name created

--- src/components/TodayWeather.tsx
+++ src/components/TodayWeather.tsx
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
+import { IState } from "../types/allTypes";
+
 const TodayWeather = () => {
+    const { currentCity } = useSelector((state: IState) => state.weatherReducer);
     return(
         <div>
-            <h3>City name</h3>
+            <h3>City name: {currentCity?.name}</h3>
             <p>Today</p>
             <div>
                 <span>date</span>
