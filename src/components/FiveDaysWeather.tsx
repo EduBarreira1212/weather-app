e929e9ab8f7752354c424870476cd93e7a3bbe58
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-19 11:40:01 -0300

    feat: five days weather component created

---
+++ src/components/FiveDaysWeather.tsx
@@ -0,0 +1,39 @@
+const FiveDaysWeather = () => {
+    return(
+        <div>
+            <h4>Next 5 days</h4>
+            <div>
+                <span>date</span>
+                <span>min</span>
+                <span>max</span>
+                <span>description</span>
+            </div>
+            <div>
+                <span>date</span>
+                <span>min</span>
+                <span>max</span>
+                <span>description</span>
+            </div>
+            <div>
+                <span>date</span>
+                <span>min</span>
+                <span>max</span>
+                <span>description</span>
+            </div>
+            <div>
+                <span>date</span>
+                <span>min</span>
+                <span>max</span>
+                <span>description</span>
+            </div>
+            <div>
+                <span>date</span>
+                <span>min</span>
+                <span>max</span>
+                <span>description</span>
+            </div>
+        </div>
+    );
+}
+
+export default FiveDaysWeather;
