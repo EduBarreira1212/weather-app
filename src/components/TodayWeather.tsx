b08c6354dd96333ced73951287b2115b4b8dc0e9
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-19 11:33:05 -0300

    feat: today weather component created

---
+++ src/components/TodayWeather.tsx
@@ -0,0 +1,16 @@
+const TodayWeather = () => {
+    return(
+        <div>
+            <h3>City name</h3>
+            <p>Today</p>
+            <div>
+                <span>date</span>
+                <span>min</span>
+                <span>max</span>
+                <span>description</span>
+            </div>
+        </div>
+    );
+}
+
+export default TodayWeather;
