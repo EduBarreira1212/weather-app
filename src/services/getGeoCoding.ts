a39360384d6d7476492c4f4edbb821c8970d1956
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-19 20:00:59 -0300

    refactor: type added

--- src/services/getGeoCoding.ts
+++ src/services/getGeoCoding.ts
@@ -1,10 +1,11 @@
 import axios from "axios"
+import { ICityList } from "../types/allTypes";
 
 const apiKey = process.env.REACT_APP_API_KEY;
  
 const getGeoCoding = async (city: string) => {
     try {
-        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
+        const response = await axios.get<ICityList>(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
         return response.data;
     } catch (error) {
         console.log("Error fetching data:", error);
