6da584e59d5e0c56cfad81b47d809c064691d3e8
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-19 11:33:43 -0300

    feat: today weather component used

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Input from './components/Input';
+import TodayWeather from './components/TodayWeather';
 
 function App() {
   return (
     <div>
       <Header/>
       <Input/>
+      <TodayWeather/>
     </div>
   );
 }
