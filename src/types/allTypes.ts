ddc4b404a923f7b3c673b039ed847efe19361a83
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-19 20:01:28 -0300

    feat: types file created

---
+++ src/types/allTypes.ts
@@ -0,0 +1,15 @@
+export interface IAction {
+    type: string
+    payload: any
+}
+
+export interface ICity {
+    country: string
+    lat: number
+    local_names: {}
+    lon: number
+    name: string
+    state: string
+}
+
+export type ICityList = ICity[] | any
