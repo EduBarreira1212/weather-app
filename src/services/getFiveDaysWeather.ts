8a6bd7b649d9b9745ce4d6cd7724dd06cd502114
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-21 11:39:38 -0300

    feat: logic to filter api response created

--- src/services/getFiveDaysWeather.ts
+++ src/services/getFiveDaysWeather.ts
@@ -1,11 +1,37 @@
 import axios from "axios";
+import { IApiResponse, IFilteredForecast, IForecast } from "../types/allTypes";
 
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const getFiveDaysWeather = async (lat: number, lon: number) => {
     try {
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-        return response.data;
+        const response = await axios.get<IApiResponse>(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+        
+        const forecasts: IForecast[] = response.data.list;
+
+        const dailyForecastMap = new Map<string, IFilteredForecast>();
+
+        forecasts.forEach((forecast: IForecast) => {
+            const date = forecast.dt_txt.split(' ')[0];
+
+            if (dailyForecastMap.has(date)) {
+                const existingForecast = dailyForecastMap.get(date)!;
+                existingForecast.temp_min = Math.min(existingForecast.temp_min, forecast.main.temp_min);
+                existingForecast.temp_max = Math.max(existingForecast.temp_max, forecast.main.temp_max);
+            } else {
+                dailyForecastMap.set(date, {
+                    date,
+                    temp_min: forecast.main.temp_min,
+                    temp_max: forecast.main.temp_max,
+                    main: forecast.weather[0].main
+                });
+            }
+        });
+
+        const filteredForecasts = Array.from(dailyForecastMap.values());
+
+        return filteredForecasts;
+
     } catch (error) {
         console.log("Error fetching data:", error);
     }
