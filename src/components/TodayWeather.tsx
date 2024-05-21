b7ce1791598dfba0fab2fcbe9a4e8fc8cd8d1719
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-21 11:34:05 -0300

    feat: new render method with reducer values

--- src/components/TodayWeather.tsx
+++ src/components/TodayWeather.tsx
@@ -3,16 +3,22 @@ import { IState } from "../types/allTypes";
 
 const TodayWeather = () => {
     const { currentCity } = useSelector((state: IState) => state.weatherReducer);
+    const { currentWeather } = useSelector((state: IState) => state.weatherReducer);
     return(
         <div>
             <h3>City name: {currentCity?.name}</h3>
             <p>Today</p>
-            <div>
-                <span>date</span>
-                <span>min</span>
-                <span>max</span>
-                <span>description</span>
-            </div>
+                {currentWeather && currentWeather.length > 0 ? (
+                    <div>
+                        <span>date: {currentWeather[0].date}</span>
+                        <span>min: {currentWeather[0].temp_min} ºC</span>
+                        <span>max: {currentWeather[0].temp_max} ºC</span>
+                        <span>{currentWeather[0].main}</span>
+                    </div>
+                    
+                ) : (
+                    <span>No weather data available</span>
+                )}
         </div>
     );
 }
