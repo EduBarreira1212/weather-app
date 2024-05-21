81eea2cacf48c9b27ce6914b5b6109809a92b098
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-21 09:45:17 -0300

    fix: fixing url error

--- src/services/getFiveDaysWeather.ts
+++ src/services/getFiveDaysWeather.ts
@@ -4,7 +4,7 @@ const apiKey = process.env.REACT_APP_API_KEY;
 
 const getFiveDaysWeather = async (lat: number, lon: number) => {
     try {
-        const response = await axios.get(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
         return response.data;
     } catch (error) {
         console.log("Error fetching data:", error);
