93d4fe7937e6d822e95068f59429c2e85c44d623
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-21 11:30:55 -0300

    feat: new case and action added

--- src/redux/weatherReducer/actions.ts
+++ src/redux/weatherReducer/actions.ts
@@ -1,7 +1,12 @@
-import { ICity, IAction, ICityList } from "../../types/allTypes";
+import { IAction, ICityList, IFilteredForecast } from "../../types/allTypes";
 import weatherActionTypes from "./action-types";
 
 export const setCityAction = (city: ICityList): IAction => ({
     type: weatherActionTypes.SET_CITY,
     payload: city[0]
+})
+
+export const setWeatherAction = (weather: IFilteredForecast[]):IAction => ({
+    type: weatherActionTypes.SET_WEATHER,
+    payload: weather
 })
--- src/redux/weatherReducer/reducer.ts
+++ src/redux/weatherReducer/reducer.ts
@@ -3,12 +3,15 @@ import weatherActionTypes from "./action-types";
 
 const initialState = {
     currentCity: null,
+    currentWeather: null,
 };
 
 const weatherReducer = (state = initialState, action: IAction) => {
     switch (action.type) {
         case weatherActionTypes.SET_CITY:
             return { ...state, currentCity: action.payload };
+        case weatherActionTypes.SET_WEATHER:
+            return { ...state, currentWeather: action.payload };
         default:
             return state;
     }
