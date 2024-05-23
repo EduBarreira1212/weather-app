fe17ffff5b6326923d5de31903700462159ad2ec
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-23 11:50:34 -0300

    feat: styled components added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,6 +3,33 @@ import getGeoCoding from "../services/getGeoCoding";
 import { setCityAction, setWeatherAction } from "../redux/weatherReducer/actions";
 import store from "../redux/store";
 import getFiveDaysWeather from "../services/getFiveDaysWeather";
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Label = styled.label`
+    font-size: medium;
+`;
+
+const StyledInput = styled.input`
+    padding: 0.5vh 0.5vw;
+    font-size: medium;
+`;
+
+const Button = styled.button`
+    padding: 0.65vh 0.5vw;
+    font-size: medium;
+    cursor: pointer;
+    background-color: #00BFA6;
+    border: none;
+    &:hover {
+      background-color: #00897B;
+    }
+`;
 
 const Input = () => {
 
@@ -17,11 +44,13 @@ const Input = () => {
         store.dispatch(setWeatherAction(weather!));
     }
     return(
-        <div>
-            <label>Insert a city</label>
-            <input ref={cityRef} type='text' placeholder='Ex: London'/>
-            <button onClick={handleClick}>Search</button>
-        </div>
+        <FormContainer>
+            <Label>Insert a city</Label>
+            <div>
+                <StyledInput ref={cityRef} type='text' placeholder='Ex: London'/>
+                <Button onClick={handleClick}>Search</Button>
+            </div>
+        </FormContainer>
     );
 }
 
