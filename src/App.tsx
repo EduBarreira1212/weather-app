7b957f96a372f345641832ce04f9fe22fc54dd1f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-23 11:09:48 -0300

    feat: styled div added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import styled from 'styled-components';
 import FiveDaysWeather from './components/FiveDaysWeather';
 import Header from './components/Header';
 import Input from './components/Input';
 import TodayWeather from './components/TodayWeather';
 
+const Div = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  background-color: #1A0E4A;
+  color: white;
+`
+
 function App() {
   return (
-    <div>
+    <Div>
       <Header/>
       <Input/>
       <TodayWeather/>
       <FiveDaysWeather/>
-    </div>
+    </Div>
   );
 }
 
