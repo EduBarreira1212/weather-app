f28411ea06dc337c6788dfde9919d25cc07e0126
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-24 20:40:28 -0300

    refactor: using map to render components

--- src/components/FiveDaysWeather.tsx
+++ src/components/FiveDaysWeather.tsx
@@ -35,36 +35,14 @@ const FiveDaysWeather = () => {
             <h4>Next 5 days</h4>
             {currentWeather ? (
                 <div>
-                    <Div2>
-                        <Span>{currentWeather[1].date}</Span>
-                        <Span>min: {currentWeather[1].temp_min} ºC</Span>
-                        <Span>max: {currentWeather[1].temp_max} ºC</Span>
-                        <Span>{currentWeather[1].main}</Span>
+                    {currentWeather.map((weather, index) => (
+                    <Div2 key={index}>
+                        <Span>{weather.date}</Span>
+                        <Span>min: {weather.temp_min} ºC</Span>
+                        <Span>max: {weather.temp_max} ºC</Span>
+                        <Span>{weather.main}</Span>
                     </Div2>
-                    <Div2>
-                        <Span>{currentWeather[2].date}</Span>
-                        <Span>min: {currentWeather[2].temp_min} ºC</Span>
-                        <Span>max: {currentWeather[2].temp_max} ºC</Span>
-                        <Span>{currentWeather[2].main}</Span>
-                    </Div2>
-                    <Div2>
-                        <Span>{currentWeather[3].date}</Span>
-                        <Span>min: {currentWeather[3].temp_min} ºC</Span>
-                        <Span>max: {currentWeather[3].temp_max} ºC</Span>
-                        <Span>{currentWeather[3].main}</Span>
-                    </Div2>
-                    <Div2>
-                        <Span>{currentWeather[4].date}</Span>
-                        <Span>min: {currentWeather[4].temp_min} ºC</Span>
-                        <Span>max: {currentWeather[4].temp_max} ºC</Span>
-                        <Span>{currentWeather[4].main}</Span>
-                    </Div2>
-                    <Div2>
-                        <Span>{currentWeather[5].date}</Span>
-                        <Span>min: {currentWeather[5].temp_min} ºC</Span>
-                        <Span>max: {currentWeather[5].temp_max} ºC</Span>
-                        <Span>{currentWeather[5].main}</Span>
-                    </Div2>
+                ))}
                 </div>
             ) : (
                 <Span>No weather data available</Span>
