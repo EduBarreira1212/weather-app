d77f5f911429a42ef4403fca12d3cda4650e4be6
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-24 21:35:36 -0300

    fix: adding min height

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Input from './components/Input';
 import TodayWeather from './components/TodayWeather';
 
 const Div = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
