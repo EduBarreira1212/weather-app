eb2d99030cd40fe88806b78b902211f80588f186
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-25 21:07:24 -0300

    feat: interfaces to new api response created

--- src/types/allTypes.ts
+++ src/types/allTypes.ts
@@ -18,6 +18,7 @@ export interface IState {
     weatherReducer: {
         currentCity: ICity
         currentWeather: IFilteredForecast[]
+        currentTodayWeather: IFilteredTodayApiResponse
     }
 }
 
@@ -76,4 +77,71 @@ export interface IFilteredForecast {
     temp_min: number;
     temp_max: number;
     main: string;
+}
+
+export interface ITodayWeatherApiResponse {
+    coord: {
+      lon: number;
+      lat: number;
+    };
+    weather: Array<{
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }>;
+    base: string;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+      sea_level?: number;
+      grnd_level?: number;
+    };
+    visibility: number;
+    wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+    };
+    rain?: {
+      "1h": number;
+    };
+    clouds: {
+      all: number;
+    };
+    dt: number;
+    sys: {
+      type: number;
+      id: number;
+      country: string;
+      sunrise: number;
+      sunset: number;
+    };
+    timezone: number;
+    id: number;
+    name: string;
+    cod: number;
+}
+
+export interface IFilteredTodayApiResponse {
+    weather: Array<{
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }>;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+      sea_level?: number;
+      grnd_level?: number;
+    };
 }
