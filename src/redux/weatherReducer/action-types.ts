d2987580ef72717452600d68b061d42a1d50f87c
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-08 15:12:15 -0300

    refactor: changing action types names

--- src/redux/weatherReducer/action-types.ts
+++ src/redux/weatherReducer/action-types.ts
@@ -1,7 +1,7 @@
 const weatherActionTypes = {
     SET_CITY: "weather/SET_CITY",
     SET_WEATHER: "weather/SET_WEATHER",
-    SET_TODAY_WEATHER: "weather/SET_TODAY_WEATHER"
+    SET_FORECAST: "weather/SET_FORECAST"
 }
 
 export default weatherActionTypes;
